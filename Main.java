d99cc2e14534d4f84b7d54978701b755e8683cd3
ChaseBourn <36450008+ChaseBourn@users.noreply.github.com>
2018-02-13 11:30:56 -0500

    Add files via upload
    new version with vladimir comment credit

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // CREDIT TO VLADIMIR FOR HELPING US : )
 
         Scanner scanner = new Scanner(System.in);
 
